a9314f512adf490e55a0865d5c8dc067474bcb22
feefeefee <feiqikai@qq.com>
2023-04-29 16:51:39 +0800

    chore: base配置

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const name = ref("大西瓜");
+</script>
+<template>
+  <div class="name">{{ name }}哈哈哈哈</div>
+</template>
+<style scoped less>
+.name {
+  font-size: 24px;
+}
+</style>
